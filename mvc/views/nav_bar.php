65daee6dc3330031c556bc33475fd23ec3f86dde
trannguyenhan <trannguyenhan01092000@gmail.com>
2021-05-15 01:06:01 +0700

    fix path

--- mvc/views/nav_bar.php
+++ mvc/views/nav_bar.php
@@ -3,11 +3,11 @@
 ?>
 <div class="nav_bar">
 	<div class="logo">
-		<a href="home"><img src="https://i0.wp.com/s1.uphinh.org/2021/04/25/Untitled-Design-3.png" width="125px"></a>
+		<a href=<?php echo "/" . $path_project . "/" ?>><img src="https://i0.wp.com/s1.uphinh.org/2021/04/25/Untitled-Design-3.png" width="125px"></a>
 	</div>
 	<nav>
 		<ul>
-			<li><a href=<?php echo "/" . $path_project . "/" . "home" ?>><span>Trang chủ</span></a></li>
+			<li><a href=<?php echo "/" . $path_project . "/"?>><span>Trang chủ</span></a></li>
 			<li><a href=<?php echo "/" . $path_project . "/" . "about" ?>>Về chúng tôi</a></li>
 			<li><a href=<?php echo "/" . $path_project . "/" . "products" ?>>Sản phẩm</a></li>
 			<li><a href=<?php echo "/" . $path_project . "/" . "contact" ?>>Liên hệ</a></li>
