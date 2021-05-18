724759ac0e9ebf351c34b9bda60a6ff9a3f24cb7
Tran Minh <quangminhtran22@gmail.com>
2021-05-18 16:33:32 +0700

    add account.php

--- mvc/views/account.php
+++ mvc/views/account.php
@@ -1,4 +1,22 @@
-<?php ?>
+<?php
+		if(array_key_exists("username", $_POST)){
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+				$address = $_POST['address'];
+				$name_1 = $_POST['name_1'];
+				$name_2 = $_POST['name_2'];
+				$name = $name_1 . " " . $name_2;
+				$telephone = $_POST['telephone'];
+
+				require_once ROOT . DS .'services' . DS . 'GuestServices.php';
+				require_once ROOT . DS . 'mvc' . DS . 'models' . DS . 'Guest.php';
+
+				$guest = new Guest($username, $password, $name, $address, $telephone);
+				$service = new GuestServices();
+				$service->insert($guest);
+
+		}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,29 +34,32 @@
 
 	<div class="container">
 		<form id = "form1">
-			<h3>CREATE ACCOUNT</h3>
-			<input type="text" name="" placeholder="Email" required>
-			<input type="password" name="" placeholder="Password" required>
-			<input type="password" name="" placeholder="Confirm Password" required>
+			<h3>TẠO TÀI KHOẢN</h3>
+			<input type="text" name="username" placeholder="Username" id="username" title="usename không quá 50 kí tự và không được để trống" required>
+			<input type="password" name="password" placeholder="Password" title="password không quá 50 kí tự và không được để trống" id="password" required>
+			<input type="password" name="check_password" placeholder="Confirm Password" id="check_password" required>
 			<div class="btn-box">
 				<button type = "button" id = "next1">Next</button>
 			</div>
 		</form>
 		<form id = "form2">
-			<h3>SOCIAL LINKS</h3>
-			<input type="text" name="" placeholder="GoogleDrive">
-			<input type="text" name="" placeholder="Github">
-			<input type="text" name="" placeholder="Linkedin">
+			<h3>ĐỊA CHỈ</h3>
+			<input type="text" name="add_1" id="add_1" placeholder="Tỉnh">
+			<input type="text" name="add_2" id="add_2" placeholder="Huyện">
+			<input type="text" name="add_3" id="add_3" placeholder="Xã">
 			<div class="btn-box">
 				<button type = "button" id = "back1">Back</button>
 				<button type = "button" id = "next2">Next</button>
 			</div>
 		</form>
-		<form id = "form3">
-			<h3>PERSONAL INFO</h3>
-			<input type="text" name="" placeholder="First Name">
-			<input type="text" name="" placeholder="Last Name">
-			<input type="text" name="" placeholder="Moblie No.">
+		<form id = "form3" action="" method="POST">
+			<h3>THÔNG TIN CÁ NHÂN</h3>
+			<input type="text" name="username" id="user_name" style="display: none">
+			<input type="password" name="password" id="your_password" style="display:none">
+			<input type="text" name="address" id="address" style="display:none">
+			<input type="text" name="name_1" placeholder="Tên">
+			<input type="text" name="name_2" placeholder="Họ và tên đệm">
+			<input type="text" name="telephone" placeholder="SĐT">
 			<div class="btn-box">
 				<button type = "button" id = "back2">Back</button>
 				<button type = "submit">Submit</button>
@@ -66,21 +87,78 @@
 		var Back1 = document.getElementById('back1');
 		var Back2 = document.getElementById('back2');
 		var Progress = document.getElementById('progress');
+		// form 1
 		Next1.onclick= function () {
-			Form1.style.left = "-450px";
-			Form2.style.left = "40px";
-			Progress.style.width = "240px";
+			// get username, password, re-password
+			let password = document.getElementById("password").value;
+			let check_password = document.getElementById("check_password").value;
+			let username = document.getElementById("username").value;
+
+			// check username have length character between 0 and 50
+			if(username === "" || username.length > 50){
+					document.getElementById("username").style.border = "1px solid red"; // set border color to red
+					return;
+			}
+
+			// check password have length character between 0 and 50
+			if(password === "" || password.length > 50){
+					document.getElementById("username").style.border = "";
+					document.getElementById("password").style.border = "1px solid red";
+					return;
+			}
+
+			// check re-password same with password
+			if(password === check_password){
+					Form1.style.left = "-450px";
+					Form2.style.left = "40px";
+					Progress.style.width = "240px";
+
+					document.getElementById("username").style.border = "";
+					document.getElementById("password").style.border = "";
+					document.getElementById("check_password").style.border = "";
+			} else {
+					document.getElementById("password").style.border = "1px solid red";
+					document.getElementById("check_password").style.border = "1px solid red";
+			}
 		}
+
 		Back1.onclick= function () {
 			Form1.style.left = "40px";
 			Form2.style.left = "450px";
 			Progress.style.width = "120px";
 		}
+
+		// form 2
 		Next2.onclick= function () {
+			let add_1 = document.getElementById("add_1").value;
+			let add_2 = document.getElementById("add_2").value;
+			let add_3 = document.getElementById("add_3").value;
+			let add = add_1 + " " + add_2 + " " + add_3;
+			if(add_1 === "" && add_2 === "" && add_3 ===""){ // check input of address
+					document.getElementById("add_1").style.border = "1px solid red";
+					document.getElementById("add_2").style.border = "1px solid red";
+					document.getElementById("add_3").style.border = "1px solid red";
+					return;
+			}
+
+			let password = document.getElementById("password").value;
+			let username = document.getElementById("username").value;
+			let address = add;
+
+			document.getElementById("user_name").value = username;
+			document.getElementById("your_password").value = password;
+			document.getElementById("address").value = address;
+
 			Form2.style.left = "-450px";
 			Form3.style.left = "40px";
 			Progress.style.width = "360px";
+
+			// return color for input tag if input true
+			document.getElementById("add_1").style.border = "";
+			document.getElementById("add_2").style.border = "";
+			document.getElementById("add_3").style.border = "";
 		}
+
 		Back2.onclick= function () {
 			Form2.style.left = "40px";
 			Form3.style.left = "450px";
--- mvc/views/details.php
+++ mvc/views/details.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 session_start();
 ?>
 <!DOCTYPE html>
@@ -31,7 +32,7 @@ session_start();
 		<div><b>Màu sắc</b>: <?php echo $product->getColor() ?></div>
 		<div><b>Thương hiệu</b>: <?php echo $product->getSupplier() ?></div>
 		<div><b>Mô tả sản phẩm</b>: <?php echo $product->getDescription() ?></div>
-	
+
 		<?php
 		$check = TypeProductsServices::checkType($product->getProductID());
 
@@ -46,7 +47,7 @@ session_start();
 			<div><b>Kết nối chính</b>: <?php echo $product->getMainConnection() ?></div>
 			<div><b>Hệ điều hành</b>: <?php echo $product->getOS() ?></div>
 			<?php
-			
+
 			if($check == Type::PC) {
 			?>
 				<div><b>Case</b>: <?php echo $product->getCase()?></div>
@@ -64,7 +65,7 @@ session_start();
 		?>
 			<div><b>Kết nối tiêu chuẩn</b>: <?php echo $product->getStandardConnection() ?></div>
 			<div><b>Giao thức kết nối</>: <?php echo $product->getConnectionProtocon() ?></div>
-			<div><b>Có led</b>: 
+			<div><b>Có led</b>:
 			<?php
 			if($product->getIsLed()) {
 				echo "Có";
@@ -80,53 +81,65 @@ session_start();
 	?>
 	</div>
 </div>
-
+<form action="" method="POST">
+<div class='add-product'>
+	<input class='number' type='number' name='number' min='1' >
+	<input class='add' type='submit' value='Thêm vào giỏ hàng' >
+</div>
+</form>
 <!-- allow user to evaluate -->
 <div class='cmt-title'><b>Đánh giá của bạn</b></div>
-<form action="details.php" method="POST">
+<form action="" method="POST">
 <div class='your-evaluate'>
 	<div class='your-star'><b>Điểm đánh giá</b>: <input class='rating' type="number" name="star" min="1" max="5"></div>
 	<div class='your-cmt'><b>Bình luận</b>: </div>
 	<textarea class='comment' rows='1' name="cmt" placeholder='Hãy để lại bình luận của bạn'></textarea>
 	<input class='submit' type='submit' value='Gửi'>
 </div>
+</form>
+<?php
 
+function alert($msg) {
+	echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+
+function formatMoney($money) {
+
+}
+
+?>
 <?php
 // if click submit
-if(isset($_POST['submit'])) {
+if(array_key_exists("cmt", $_POST)) {
+
 	$star = isset($_POST['star']) ? $_POST['star'] : '';
 	$cmt = isset($_POST['cmt']) ? $_POST['cmt'] : '';
-	$username = $_SESSION['username'];
 	date_default_timezone_set('Asia/Ho_Chi_Minh');
 	$date = date('Y-m-d h:i:s', time());
 	$productID = $product->getProductID();
-	// if not log in
-	if($username == null) {
-		echo "	<script language='javascript'>
-		 			alert('Vui lòng đăng nhập để đánh giá!')
-				</script>";	
+
+	if($star == '') { // if not rating
+		alert("Vui lòng nhập điểm đánh giá!");
 	}
-	// if not rating
-	elseif($star == '') {
-		echo "	<script language='javascript'>
-		 			alert('Vui lòng nhập điểm đánh giá!')
-				</script>";
+	elseif($cmt == '') { // if not comment
+		alert('Vui lòng nhập bình luận!');
+	} 
+	elseif(!isset($_SESSION['username'])) { // if not log in
+		alert('Vui lòng đăng nhập để đánh giá!');
 	}
-	// if not comment
-	elseif($cmt == '') {
-		echo "	<script language='javascript'>
-					alert('Vui lòng nhập bình luận!')
-   				</script>";
-	}
-	// insert to database
-	else {
+	else { 	// insert to database
+		require_once ROOT . DS . 'services' . DS . 'EvaluateServices.php';
+		require_once ROOT . DS . 'mvc' . DS . 'models' . DS . 'Evaluate.php';
+
+		$username = $_SESSION['username'];
 		$evalate = new Evaluate($username, $productID, $star, $cmt, $date);
-		$eS = new EvaluateServices();
-		$eS.insert($evalate);
+		$service = new EvaluateServices();
+		$service->insert($evalate);
+		echo header('location: details.php');
 	}
 }
 ?>
-</form>
+
 <!-- print all evaluate -->
 <div class='cmt-title'><b>Ý kiến đánh giá</b></div>
 <?php
