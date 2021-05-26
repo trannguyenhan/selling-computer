15efc222062128a7fe3d6831bbf94dff82041faa
trannguyenhan <trannguyenhan01092000@gmail.com>
2021-05-27 01:36:57 +0700

    m

--- library/refresh_session.php
+++ library/refresh_session.php
@@ -6,6 +6,8 @@ if(array_key_exists("username", $_SESSION)){
     unset($_SESSION['username']);
     unset($_SESSION['password']);
     header("Location: ../login");
+
+    return;
 }
 
 if(array_key_exists("admin_username", $_SESSION)){
